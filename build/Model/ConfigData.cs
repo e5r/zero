2c61601dc0c17740a320df25aec017d8843d8938
Erlimar Silva Campos <erlimar@gmail.com>
2021-07-13 13:32:52 -0300

    Adiciona suporte a cobertura de código no build

--- build/Model/ConfigData.cs
+++ build/Model/ConfigData.cs
@@ -5,6 +5,7 @@ public class ConfigFile
     public string SolutionFile { get; set; }
     public string[] PublishGlobalExcludes { get; set; }
     public Artifact[] PublishArtifacts { get; set; }
+    public Artifact[] TestArtifacts { get; set; }
 
     public static ConfigFile Default => _defaultInstance ?? (_defaultInstance = new ConfigFile());
 }
--- build/Tasks/TestTask.cs
+++ build/Tasks/TestTask.cs
@@ -1,18 +1,49 @@
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Test;
+using Cake.Core.Diagnostics;
 using Cake.Frosting;
 
 [TaskName("Test")]
 [Dependency(typeof(BuildTask))]
 public sealed class TestTask : FrostingTask<Context>
 {
+    internal const string TestDirectory = "test";
+
     public override void Run(Context context)
     {
-        context.DotNetCoreTest(context.ConfigFile.SolutionFile, new DotNetCoreTestSettings
+        var testArtifacts = context.ConfigFile.TestArtifacts ?? Enumerable.Empty<Artifact>();
+
+        if (!testArtifacts.Any())
         {
-            Configuration = context.BuildConfiguration,
-            NoBuild = true
-        });
+            context.Log.Warning("No test artifacts entered in configuration file");
+        }
+
+        int magic = 0;
+
+        Parallel.ForEach(testArtifacts, () => 0, (testArtifact, loopState, localSum) =>
+        {
+            context.Log.Information("Testing component {0}", testArtifact.Component);
+
+            context.DotNetCoreTest($"{TestDirectory}/{testArtifact.Component}/{testArtifact.Component}.csproj", new DotNetCoreTestSettings
+            {
+                Configuration = context.BuildConfiguration,
+                NoBuild = true,
+                Collectors = new[] { "XPlat Code Coverage" }
+            });
+
+            return ++localSum;
+        }, r => Interlocked.Add(ref magic, r));
+
+        if (magic != testArtifacts.Count())
+        {
+            context.Log.Warning("Processing of parallel tasks does not seem to have been successful");
+        }
     }
 }
